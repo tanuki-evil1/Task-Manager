dd0899ffdfc3b06b406dae0f07e5e6998951563e
Tanuki Evil <tanuki.evil@yandex.ru>
2024-07-23 14:58:04 +0300

    add tests

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -8,11 +8,11 @@ class UserTemplateTests(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = User.objects.create(
-            username='testuser',
-            password='testpassword',
+        self.user = User.objects.create_user(
             first_name='Test',
-            last_name='User'
+            last_name='User',
+            username='testuser',
+            password='testpassword'
         )
 
     def test_user_list_template(self):
@@ -34,32 +34,33 @@ class UserTemplateTests(TestCase):
             'first_name': 'New',
             'last_name': 'User'
         })
-        self.assertEqual(response.status_code, 302)  # Redirect after creation
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(User.objects.count(), 2)
 
     def test_user_update_template(self):
-        assert self.client.alogin(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
         response = self.client.get(reverse('users_update', args=[self.user.pk]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/update.html')
         self.assertIsInstance(response.context['form'], UserCreateForm)
 
-        response = self.client.post(reverse('user_update', args=[self.user.pk]), {
+        response = self.client.post(reverse('users_update', args=[self.user.pk]), {
             'username': 'updateduser',
             'password1': 'updatedpassword',
             'password2': 'updatedpassword',
             'first_name': 'Updated',
             'last_name': 'User'
         })
-        self.assertEqual(response.status_code, 302)  # Redirect after update
+        self.assertEqual(response.status_code, 302)
         self.user.refresh_from_db()
         self.assertEqual(self.user.username, 'updateduser')
 
     def test_user_delete_template(self):
+        self.client.login(username='testuser', password='testpassword')
         response = self.client.get(reverse('users_delete', args=[self.user.pk]))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/delete.html')
 
-        response = self.client.post(reverse('user_delete', args=[self.user.pk]))
-        self.assertEqual(response.status_code, 302)  # Redirect after delete
+        response = self.client.post(reverse('users_delete', args=[self.user.pk]))
+        self.assertEqual(response.status_code, 302)
         self.assertEqual(User.objects.count(), 0)
