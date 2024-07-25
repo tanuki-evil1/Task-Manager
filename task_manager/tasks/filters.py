b70acf01cbac8f8b526d0672a7084e4e47841d78
Tanuki Evil <tanuki.evil@yandex.ru>
2024-07-25 16:19:25 +0300

    add filter

---
+++ task_manager/tasks/filters.py
@@ -0,0 +1,28 @@
+from django_filters import FilterSet, ModelChoiceFilter, BooleanFilter
+from django import forms
+from .models import Task
+from task_manager.labels.models import Label
+
+
+class TaskFilter(FilterSet):
+
+    labels = ModelChoiceFilter(
+        queryset=Label.objects.all(),
+        label='Label'
+    )
+
+    own_tasks = BooleanFilter(
+        label='Только свое',
+        widget=forms.CheckboxInput,
+        method='get_own_tasks',
+    )
+
+    def get_own_tasks(self, queryset, name, value):
+        if value:
+            user = self.request.user
+            return queryset.filter(author=user)
+        return queryset
+
+    class Meta:
+        model = Task
+        fields = ['status', 'executor']
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,19 +1,23 @@
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django_filters.views import FilterView
 
 from task_manager.users.models import User
-from task_manager.mixins import AuthRequiredMixin, UserIsOwnerMixin, AuthorDeleteMixin
+from task_manager.mixins import AuthRequiredMixin, AuthorDeleteMixin
 
+from .filters import TaskFilter
 from .forms import TaskCreateForm
 from .models import Task
 
 
-class IndexView(AuthRequiredMixin, ListView):
+class IndexView(AuthRequiredMixin, FilterView):
     model = Task
+    filterset_class = TaskFilter
     template_name = 'tasks/index.html'
     context_object_name = 'tasks'
     ordering = 'id'
+    extra_context = {'title': 'Создать задачу', 'button_text': 'Показать'}
 
 
 class TaskCreateView(SuccessMessageMixin, AuthRequiredMixin, CreateView):
@@ -40,7 +44,7 @@ class TaskUpdateView(SuccessMessageMixin, AuthRequiredMixin, UpdateView):
     extra_context = {'title': 'Изменение задачи', 'button_text': 'Изменить'}
 
 
-class TaskDeleteView(AuthRequiredMixin, AuthorDeleteMixin, SuccessMessageMixin,  DeleteView):
+class TaskDeleteView(AuthRequiredMixin, AuthorDeleteMixin, SuccessMessageMixin, DeleteView):
     model = Task
     template_name = 'tasks/delete.html'
     success_url = reverse_lazy('tasks_index')
